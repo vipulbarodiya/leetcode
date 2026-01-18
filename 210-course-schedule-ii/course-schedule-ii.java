881fff7f47d8285d21f2349ee4ea73c8714da474
Vipul Barodiya <vbarodiya@conviva.com>
2026-01-18 19:52:24 +0530

    Time: 7 ms (38.19%) | Memory: 46.8 MB (75.66%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,37 +1,44 @@
 class Solution {
+    public boolean dfs(int u, List<List<Integer>> g, boolean[] visited, boolean[] path, Stack<Integer> st) {
+        visited[u] = true;
+        path[u] = true;
+        for(Integer x:g.get(u)) {
+            if(path[x]) return true;
+            if(!visited[x] && dfs(x,g,visited,path,st)) {
+                return true;
+            }
+        }
+        path[u] = false;
+        st.push(u);
+        return false;
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] in = new int[numCourses];
+        int n = numCourses;
 
         List<List<Integer>> g = new ArrayList<>();
-        for(int i=0; i<numCourses; i++) {
+        for(int i=0; i<n; i++) {
             g.add(new ArrayList<>());
         }
-
         for(int[] pr: prerequisites) {
-            in[pr[1]]++;
             g.get(pr[0]).add(pr[1]);
         }
-        Queue<Integer> q =  new ArrayDeque<>();
-        for(int i=0; i<numCourses; i++) {
-            if(in[i] == 0) {
-                q.offer(i);
-            }
-        }
-        int processed  = 0;
-        List<Integer> result = new ArrayList<>();
-        while(!q.isEmpty()) {
-            Integer c = q.poll();
-            processed++;
-            for(int n:g.get(c)) {
-                in[n]--;
-                if(in[n] == 0) {
-                    q.offer(n);
+
+        boolean[] visited = new boolean[n];
+        boolean[] path = new boolean[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                if(dfs(i, g, visited, path, st)) {
+                    return new int[]{};
                 }
             }
-            result.add(c);
         }
-        if(processed!=numCourses) return new int[]{};
-        result = result.reversed();
-        return result.stream().mapToInt(i->i).toArray();
+
+        List<Integer>result = new ArrayList<>();
+        while(!st.isEmpty()) {
+            result.add(st.pop());
+        }
+        return result.reversed().stream().mapToInt(i->i).toArray();
     }
 }
