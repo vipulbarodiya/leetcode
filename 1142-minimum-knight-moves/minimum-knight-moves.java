81458bc243a3f718d5c9eaecfe81dbfddf2d3057
Vipul Barodiya <vbarodiya@conviva.com>
2026-02-03 21:12:06 +0530

    Time: 164 ms (51.33%) | Memory: 46.8 MB (86.15%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.java
+++ 1142-minimum-knight-moves/minimum-knight-moves.java
@@ -2,14 +2,18 @@ class Solution {
     private record Cell(int x, int y){};
     public int minKnightMoves(int x, int y) {
         if(x==0 && y==0) return 0;
+        // get positive equivalent
         x = Math.abs(x);
         y = Math.abs(y);
+
         Queue<Cell> q = new ArrayDeque<>();
-        Set<Cell> visited = new HashSet<>();
+        boolean[][] visited = new boolean[305][305];
+        int offset = 2;
         q.add(new Cell(0,0));
-        visited.add(new Cell(0,0));
+        visited[offset][offset] = true;
         int moves = 0;
 
+        // keep moves only for postive direction
         List<List<Integer>> dirs = List.of(List.of(2,1), List.of(2,-1), List.of(1,2), List.of(-1, 2), List.of(-2,1), List.of(-2,1), List.of(1,-2), List.of(-1,-2));
 
 
@@ -22,15 +26,13 @@ class Solution {
                 for(int j=0; j<dirs.size(); j++) {
                     int newx = c.x + dirs.get(j).get(0);
                     int newy = c.y + dirs.get(j).get(1);
-
-                    if(newx < -2 || newy< -2 || newx > (x + 2 )|| newy >(y+2)) continue;
-
-                    Cell newCell = new Cell(newx, newy);
-
-                    if(!visited.contains(newCell)) {
-                        q.add(new Cell(newx, newy));
-                        visited.add(newCell);
+                    if (newx >= -2 && newy >= -2 && newx <= 302 && newy <= 302) {
+                        if(!visited[newx+offset][newy+offset]) {
+                            q.add(new Cell(newx, newy));
+                            visited[newx+offset][newy+offset] = true;
+                        }
                     }
+
 
                 }
             }
