6b52abdef6e95eb5ce61066158f5212787642937
Vipul Barodiya <vbarodiya@conviva.com>
2026-02-03 21:29:38 +0530

    Time: 34 ms (86.68%) | Memory: 46.5 MB (88.46%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.java
+++ 1142-minimum-knight-moves/minimum-knight-moves.java
@@ -1,34 +1,33 @@
 class Solution {
-    private record Cell(int x, int y){};
+
+    public static final int[][] dirs = new int[][]{{2,1},{2,-1},{1,2},{-1,2},{-2,1},{1,-2},{-1,-2}};
     public int minKnightMoves(int x, int y) {
         if(x==0 && y==0) return 0;
-        // get positive equivalent
         x = Math.abs(x);
         y = Math.abs(y);
 
-        Queue<Cell> q = new ArrayDeque<>();
+        Queue<Integer> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[305][305];
         int offset = 2;
-        q.add(new Cell(0,0));
+        q.add(0|0);
         visited[offset][offset] = true;
         int moves = 0;
-
-        // keep moves only for postive direction
-        List<List<Integer>> dirs = List.of(List.of(2,1), List.of(2,-1), List.of(1,2), List.of(-1, 2), List.of(-2,1), List.of(-2,1), List.of(1,-2), List.of(-1,-2));
 
 
         while(!q.isEmpty()) {
             int s = q.size();
             for(int i=0; i<s; i++) {
-                Cell c = q.poll();
-                if(c.x == x && c.y==y) return moves;
+                Integer c = q.poll();
+                int ux = c>>10;
+                int uy = c&1023;
+                if(ux == x && uy==y) return moves;
 
-                for(int j=0; j<dirs.size(); j++) {
-                    int newx = c.x + dirs.get(j).get(0);
-                    int newy = c.y + dirs.get(j).get(1);
+                for(int j=0; j<dirs.length; j++) {
+                    int newx = ux + dirs[j][0];
+                    int newy = uy + dirs[j][1];
                     if (newx >= -2 && newy >= -2 && newx <= 302 && newy <= 302) {
                         if(!visited[newx+offset][newy+offset]) {
-                            q.add(new Cell(newx, newy));
+                            q.add((newx<<10) | (newy &1023));
                             visited[newx+offset][newy+offset] = true;
                         }
                     }
