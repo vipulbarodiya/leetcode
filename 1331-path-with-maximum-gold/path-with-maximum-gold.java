909e3a11dab57a341716dcab8aa46c630f4de442
Vipul Barodiya <vbarodiya@conviva.com>
2026-02-26 02:13:04 +0530

    Time: 85 ms (38.74%) | Memory: 43.2 MB (44.70%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -15,37 +15,36 @@ class Solution {
         }
         return true;
     }
-    int ans = 0;
-    int gold = 0;
-    private void dfs(int r, int c, int[][] grid, boolean[][] vis) {
-        vis[r][c] = true;
-        gold += grid[r][c];
+    private int dfs(int r, int c, int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if(r>=m || c>=n || r<0 || c<0 || grid[r][c] == 0) {
+            return 0;
+        }
+        int origin = grid[r][c];
+        grid[r][c] = 0;
+        int localMax = 0;
         for(int i=0; i<4; i++) {
             int nx = r+dirs[i][0];
             int ny = c+dirs[i][1];
 
-            if(isValid(nx,ny, grid, vis)) {
-                dfs(nx,ny,grid,vis);
-            }
+            localMax = Math.max(localMax,dfs(nx,ny,grid));
 
         }
-        ans = Math.max(ans, gold);
-        gold  -= grid[r][c];
-        vis[r][c] = false;
+        grid[r][c] = origin;
+        return origin+localMax;
     }
     public int getMaximumGold(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] vis = new boolean[m][n];
+        int maxGold = 0;
         for(int i=0; i<m; i++) {
             for(int j = 0; j<n; j++) {
-
-                if(isValid(i,j,grid,vis)) {
-                    dfs(i,j, grid, vis);
-                    ans = Math.max(ans, gold);
+                if(grid[i][j] > 0) {
+                    maxGold = Math.max(maxGold, dfs(i,j, grid));
                 }
             }
         }
-        return ans;
+        return maxGold;
     }
 }
